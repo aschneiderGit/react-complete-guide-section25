ca0ce35d342dad43a90205c86ceeda7e773199e3
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-12-04 15:17:40 +0100

    use OCntext Api isntad of redux (but productsList are high frequency change 😌)

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,31 +1,27 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-
+import React, {useContext} from 'react';
+import {ProductsContext} from '../../context/product-context';
 import Card from '../UI/Card';
 import './ProductItem.css';
-import { toggleFav } from '../../store/actions/products';
 
-const ProductItem = props => {
-  const dispatch = useDispatch();
+const ProductItem = (props) => {
+	const toggleFavFunction = useContext(ProductsContext).toggleFav;
+	const toggleFavHandler = () => {
+		toggleFavFunction(props.id);
+	};
 
-  const toggleFavHandler = () => {
-    dispatch(toggleFav(props.id));
-  };
-
-  return (
-    <Card style={{ marginBottom: '1rem' }}>
-      <div className="product-item">
-        <h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
-        <p>{props.description}</p>
-        <button
-          className={!props.isFav ? 'button-outline' : ''}
-          onClick={toggleFavHandler}
-        >
-          {props.isFav ? 'Un-Favorite' : 'Favorite'}
-        </button>
-      </div>
-    </Card>
-  );
+	return (
+		<Card style={{marginBottom: '1rem'}}>
+			<div className="product-item">
+				<h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
+				<p>{props.description}</p>
+				<button
+					className={!props.isFav ? 'button-outline' : ''}
+					onClick={toggleFavHandler}>
+					{props.isFav ? 'Un-Favorite' : 'Favorite'}
+				</button>
+			</div>
+		</Card>
+	);
 };
 
 export default ProductItem;
